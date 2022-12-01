9a3a426123e0584db7a3be211c29110282451010
Rasmus Henriksson <rasmus@boxdragon.se>
2022-12-01 09:08:02 +0100

    Updated folder structure

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,12 @@
-use std::fs;
+use std::env;
+
+mod day1;
 
 fn main() {
-    let contents = fs::read_to_string("src/input.txt").expect("No file");
-    println!("Task1: {}", calculate_max_calories(contents.clone()));
-    println!("Task2: {}", calculate_top_three_calories(contents));
-}
-
-fn calculate_max_calories(input: String) -> i32 {
-    *get_sorted_calories(input).last().unwrap()
-}
-
-fn calculate_top_three_calories(input: String) -> i32 {
-    get_sorted_calories(input)
-        .iter()
-        .rev()
-        .take(3)
-        .fold(0, |acc, val| acc + val)
-}
-
-fn get_sorted_calories(input: String) -> Vec<i32> {
-    let mut elfs = vec![0];
-    for c in input.split('\n').into_iter() {
-        if c.is_empty() {
-            // Seperator, beginning of new elf storage
-            elfs.push(0);
-            continue;
+    for arg in env::args().into_iter() {
+        match arg.as_str() {
+            "1" => day1::main(),
+            _ => {}
         }
-
-        if let Ok(val) = c.parse::<i32>() {
-            *elfs.last_mut().unwrap() += val;
-        }
-    }
-    elfs.sort();
-    elfs
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    fn get_test_input() -> String {
-        String::from("1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000")
-    }
-
-    #[test]
-    fn task1() {
-        let expected_outcome = 24000;
-        assert_eq!(calculate_max_calories(get_test_input()), expected_outcome);
-    }
-
-    #[test]
-    fn task2() {
-        let expected_outcome = 24000 + 11000 + 10000;
-        assert_eq!(
-            calculate_top_three_calories(get_test_input()),
-            expected_outcome
-        );
     }
 }
